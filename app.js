aa14cb5497ff0d02479f087d8dd310b1d47e0d9a
v_yuzhuang01 <v_yuzhuang01@baidu.com>
2019-04-29 19:54:44 +0800

    爬虫:sunny:

---
+++ app.js
@@ -0,0 +1,47 @@
+// 这句的意思就是引入 `express` 模块，并将它赋予 `express` 这个变量等待使用。
+var express = require('express');
+var utility = require('utility');
+var superagent = require('superagent');
+var cheerio = require('cheerio');
+// 调用 express 实例，它是一个函数，不带参数调用时，会返回一个 express 实例，将这个变量赋予 app 变量。
+var app = express();
+// app 本身有很多方法，其中包括最常用的 get、post、put/patch、delete，在这里我们调用其中的 get 方法，为我们的 `/` 路径指定一个 handler 函数。
+// 这个 handler 函数会接收 req 和 res 两个对象，他们分别是请求的 request 和 response。
+// request 中包含了浏览器传来的各种信息，比如 query 啊，body 啊，headers 啊之类的，都可以通过 req 对象访问到。
+// res 对象，我们一般不从里面取信息，而是通过它来定制我们向浏览器输出的信息，比如 header 信息，比如想要向浏览器输出的内容。这里我们调用了它的 #send 方法，向浏览器输出一个字符串。
+app.get('/', function (req, res) {
+  // 从 req.query 中取出我们的 q 参数。
+  // 如果是 post 传来的 body 数据，则是在 req.body 里面，不过 express 默认不处理 body 中的信息，需要引入 https://github.com/expressjs/body-parser 这个中间件才会处理，这个后面会讲到。
+  // 如果分不清什么是 query，什么是 body 的话，那就需要补一下 http 的知识了
+  var q = req.query.q;
+  // 调用 utility.md5 方法，得到 md5 之后的值
+  // 之所以使用 utility 这个库来生成 md5 值，其实只是习惯问题。每个人都有自己习惯的技术堆栈，
+  // 我刚入职阿里的时候跟着苏千和朴灵混，所以也混到了不少他们的技术堆栈，仅此而已。
+  // utility 的 github 地址：https://github.com/node-modules/utility
+  // 里面定义了很多常用且比较杂的辅助方法，可以去看看
+  var md5Value = utility.md5(q);
+  superagent.get('https://cnodejs.org/').end(function (err, sres) {
+    //常规的错误处理
+    if (err) {
+      return next(err);
+    }
+    // sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
+    // 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
+    // 剩下就都是 jquery 的内容了
+    var $ = cheerio.load(sres.text);
+    var items = [];
+    $('#topic_list .topic_title').each(function (idx, ele) {
+      var $element = $(ele);
+      items.push({
+        title: $element.attr('title'),
+        href: $element.attr('href')
+      });
+    });
+    res.send(items);
+  });
+  // res.send(md5Value);
+})
+// 定义好我们 app 的行为之后，让它监听本地的 3000 端口。这里的第二个函数是个回调函数，会在 listen 动作成功后执行，我们这里执行了一个命令行输出操作，告诉我们监听动作已完成。
+app.listen('1234', function () {
+  console.log('app is listening at port 1234');
+})
